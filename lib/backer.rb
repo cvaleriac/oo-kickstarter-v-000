3521e51a52fac0b9cdcbd50dc6c999f2ab7462d5
valeria campilongo <cvaleriac@hotmail.it>
2018-06-07 20:40:25 +0000

    automatically backed up by learn

--- lib/backer.rb
+++ lib/backer.rb
@@ -1,13 +1,13 @@
 class Backer
+  attr_accessor :name :backed_projects
   @backed_projects = []
-  attr_accessor :name
 
   def initialize(name)
     @name = name
   end
 
   def back_project(project)
-    @backed_projects << self
-
+    @backed_projects << project
+    project.backers << self
   end
 end
